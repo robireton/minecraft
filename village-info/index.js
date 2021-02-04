ed181c8024c0ed56349914226e4dc7dc3a924125
Rob Ireton <rob@ireton.org>
2021-02-04 16:14:21 -0500

    nodejs code for village info

---
+++ village-info/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const nbt = require('prismarine-nbt')
+const level = 'World B'
+// const level = 'The World'
+// const level = 'Land & Sea · Again'
+// const level = 'Land & Sea · Trip'
+// const level = 'Coastline'
+
+fs.readFile(`/Users/rob/Library/Application Support/minecraft/saves/${level}/level.dat`, (error, data) => {
+  if (error) throw error;
+
+  nbt.parse(data, (error, data) => {
+    // console.log(JSON.stringify(data.value.data.value.Villages.value.value))
+    for (village of data.value.data.value.Villages.value.value) {
+      console.log({
+        'center': `${village.CX.value} ${village.CY.value} ${village.CZ.value}`,
+        'radius': village.Radius.value,
+        'population': village.PopSize.value,
+        'doors': village.Doors.value.value.length
+      })
+    }
+  })
+})
